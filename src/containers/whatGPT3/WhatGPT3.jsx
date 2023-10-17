2d15bd38122208af3304a84ca085371b7f336ff5
Shandea <86807815+Shandea@users.noreply.github.com>
2023-10-17 10:25:18 -0700

    feature component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './whatGPT3.css'
-import { Feature } from '../../components'
+import React from 'react';
+import './whatGPT3.css';
+import Feature  from '../../components/feature/Feature';
 
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='whtgpt3'>
       <div className='gpt3__whatgpt3-feature'> 
-        <Feature />
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
@@ -21,4 +21,4 @@ const WhatGPT3 = () => {
   )
 }
 
-export default WhatGPT3
+export default WhatGPT3;
