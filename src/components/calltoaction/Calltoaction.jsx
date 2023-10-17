336cd67a99a510710bfe8c84513eb704b455a54b
Shandea <86807815+Shandea@users.noreply.github.com>
2023-10-17 10:44:59 -0700

    calltoaction component

--- src/components/calltoaction/Calltoaction.jsx
+++ src/components/calltoaction/Calltoaction.jsx
@@ -3,9 +3,15 @@ import './calltoaction.css';
 
 const Calltoaction = () => {
   return (
-    <div>
-      Calltoaction
+    <div className="gpt3__cta">
+    <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
     </div>
+    <div className="gpt3__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
   )
 }
 
